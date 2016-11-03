87ddd3fc0ae4d55946154beaf1a50f8166b20297
Macbook <nalinsuri@gmail.com>
2016-11-03 19:05:10 -0400

    Able to get 1's place counter

--- src/RadixSortTester.java
+++ src/RadixSortTester.java
@@ -6,7 +6,7 @@ public class RadixSortTester {
 		int[] unsorted = new int[30];
 		Random rand = new Random();
 		int[] sorted = new int[30];
-		int[] temp = new int[10];
+		int[] temp = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		
 		for(int i = 0; i < unsorted.length; i++){
 			unsorted[i] = rand.nextInt(1000);
@@ -17,6 +17,53 @@ public class RadixSortTester {
 				System.out.print(unsorted[i]);
 			}
 		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < unsorted.length; i++){
+			temp[unsorted[i]%10] += 1;
+		}
+		
+		for(int i = 0; i < temp.length; i++){
+			if(i < temp.length - 1){
+				System.out.print(temp[i] + ", ");
+			}
+			else{
+				System.out.print(temp[i]);
+			}
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < temp.length; i++){
+			if(i > 0){
+				temp[i] += temp[i-1];
+			}
+			
+		}
+		
+		for(int i = 0; i < temp.length; i++){
+			if(i < temp.length - 1){
+				System.out.print(temp[i] + ", ");
+			}
+			else{
+				System.out.print(temp[i]);
+			}
+		}
+		
+
+		
+		/*
+		for(int i = unsorted.length - 1; i >= 0; i--){
+			sorted[temp[unsorted[i]%10]] = unsorted[i];
+			if(i < unsorted.length - 1){
+				System.out.print(sorted[i] + ", ");
+			}
+			else{
+				System.out.print(sorted[i]);
+			}
+		}
+		*/
 
 	}
 
