60839b4627fe03c89ec59b246d6bb06121809f2b
Macbook <nalinsuri@gmail.com>
2016-11-03 18:49:13 -0400

    Fill unsorted array

--- src/RadixSortTester.java
+++ src/RadixSortTester.java
@@ -1,7 +1,22 @@
+import java.util.Random;
+
 public class RadixSortTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] unsorted = new int[30];
+		Random rand = new Random();
+		int[] sorted = new int[30];
+		int[] temp = new int[10];
+		
+		for(int i = 0; i < unsorted.length; i++){
+			unsorted[i] = rand.nextInt(1000);
+			if(i < unsorted.length - 1){
+				System.out.print(unsorted[i] + ", ");
+			}
+			else{
+				System.out.print(unsorted[i]);
+			}
+		}
 
 	}
 
