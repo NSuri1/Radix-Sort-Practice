f9a356495ffa2802e1201e738d870026e2af0321
Macbook <nalinsuri@gmail.com>
2016-11-03 20:45:17 -0400

    Working

---
+++ src/RadixSort.java
@@ -0,0 +1,44 @@
+
+public class RadixSort {
+
+	public void sort(int[] toSort, int largestDigit) {
+		int counter = 0;
+		int divisor = 1;
+		int[] sorted = new int[toSort.length];
+		int[] temp = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+		while (counter < largestDigit) {
+
+			// Count how many of each number have certain digit
+			for (int i = 0; i < toSort.length; i++) {
+				temp[(toSort[i] / divisor) % 10] += 1;
+			}
+
+			for (int i = 0; i < temp.length; i++) {
+				if (i > 0) {
+					temp[i] += temp[i - 1];
+				}
+
+			}
+
+			// Sort
+			for (int i = toSort.length - 1; i >= 0; i--) {
+				temp[(toSort[i] / divisor) % 10] -= 1;
+				sorted[temp[(toSort[i] / divisor) % 10]] = toSort[i];
+			}
+
+			// Copy sorted array into toSort for further sorting
+			for (int i = 0; i < toSort.length; i++) {
+				toSort[i] = sorted[i];
+			}
+
+			// reset temp values to 0 to prevent out of bounds error
+			for (int i = 0; i < temp.length; i++) {
+				temp[i] = 0;
+			}
+
+			divisor *= 10;
+			counter++;
+		}
+	}
+}
--- src/RadixSortTester.java
+++ src/RadixSortTester.java
@@ -3,108 +3,34 @@ import java.util.Random;
 public class RadixSortTester {
 
 	public static void main(String[] args) {
-		int[] unsorted = new int[30];
+		int[] toSort = new int[30];
 		Random rand = new Random();
-		int[] sorted = new int[30];
-		int[] temp = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		RadixSort test = new RadixSort();
+		int upperBound = 100000;
+		int digits = Integer.toString(upperBound).length() - 1;
 
 		// Generate Random ints to fill array and print out to console
 		System.out.println("Starting Array:");
-		for (int i = 0; i < unsorted.length; i++) {
-			unsorted[i] = rand.nextInt(1000);
-			if (i < unsorted.length - 1) {
-				System.out.print(unsorted[i] + ", ");
+		for (int i = 0; i < toSort.length; i++) {
+			toSort[i] = rand.nextInt(100000);
+			if (i < toSort.length - 1) {
+				System.out.print(toSort[i] + ", ");
 			} else {
-				System.out.print(unsorted[i]);
+				System.out.print(toSort[i]);
 			}
 		}
 		System.out.println();
 
-		// Count how many of each number have certain i digit in one's place
-		for (int i = 0; i < unsorted.length; i++) {
-			temp[unsorted[i] % 10] += 1;
-		}
+		test.sort(toSort, digits);
 
-		for (int i = 0; i < temp.length; i++) {
-			if (i > 0) {
-				temp[i] += temp[i - 1];
-			}
-
-		}
-
-		// Sort only one's place
-		for (int i = unsorted.length - 1; i >= 0; i--) {
-			temp[unsorted[i] % 10] -= 1;
-			sorted[temp[unsorted[i] % 10]] = unsorted[i];
-		}
-		
-		// Copy sorted array into unsorted for further sorting
-		for (int i = 0; i < unsorted.length; i++) {
-			unsorted[i] = sorted[i];
-		}
-		
-		//reset temp values to 0 to prevent out of bounds error
-		for (int i = 0; i < temp.length; i++) {
-			temp[i] = 0;
-		}
-
-		// Count how many of each number have certain i digit in ten's place
-		for (int i = 0; i < unsorted.length; i++) {
-			temp[(unsorted[i] / 10) % 10] += 1;
-		}
-
-		for (int i = 0; i < temp.length; i++) {
-			if (i > 0) {
-				temp[i] += temp[i - 1];
-			}
-
-		}
-
-		// Sort ten's place
-		for (int i = unsorted.length - 1; i >= 0; i--) {
-			temp[(unsorted[i] / 10) % 10] -= 1;
-			sorted[temp[(unsorted[i] / 10) % 10]] = unsorted[i];
-		}
-
-		// Copy sorted array into unsorted for further sorting
-		for (int i = 0; i < unsorted.length; i++) {
-			unsorted[i] = sorted[i];
-		}
-
-		// reset temp values to 0 to prevent out of bounds error
-		for (int i = 0; i < temp.length; i++) {
-			temp[i] = 0;
-		}
-
-		// Count how many of each number have certain i digit in hundred's place
-		for (int i = 0; i < unsorted.length; i++) {
-			temp[unsorted[i] / 100] += 1;
-		}
-
-		for (int i = 0; i < temp.length; i++) {
-			if (i > 0) {
-				temp[i] += temp[i - 1];
-			}
-
-		}
-
-		// Sort hundred's place
-		for (int i = unsorted.length - 1; i >= 0; i--) {
-			temp[unsorted[i] / 100] -= 1;
-			sorted[temp[unsorted[i] / 100]] = unsorted[i];
-		}
-
-		System.out.println("Sorted Array of 3 Digit Numbers:");
-		// Display sorted hundred's place to console
-		for (int i = 0; i < sorted.length; i++) {
-			if (i < sorted.length - 1) {
-				System.out.print(sorted[i] + ", ");
+		System.out.println("Sorted Array: ");
+		for (int i = 0; i < toSort.length; i++) {
+			if (i < toSort.length - 1) {
+				System.out.print(toSort[i] + ", ");
 			} else {
-				System.out.print(sorted[i]);
+				System.out.print(toSort[i]);
 			}
 		}
-
-		System.out.println();
 
 	}
 
